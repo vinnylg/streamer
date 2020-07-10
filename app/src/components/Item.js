ec7d6cfa9b84ac38565194cdc52341a9b3c79d2d
Vinícius de Lima Gonçalves <viniciusslimaa@outlook.com>
2020-07-10 20:15:55 -0300

    first commit

---
+++ app/src/components/Item.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Item = () => {
+	return <></>
+}
+
+export default Item
---
+++ app/src/components/RecentWatched.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const RecentWatched = () => {
+	return <></>
+}
+
+export default RecentWatched
